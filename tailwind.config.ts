64c44e22d98643389adbf09c627fb21ac2bb907a
Andry Ariadi <andryariadi23@gmail.com>
2024-06-01 13:58:39 +0700

    setup tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,19 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    fontFamily: {
+      sans: ["var(--font-sora)", ...fontFamily.sans],
+      code: "var(--font-code)",
+      grotesk: "var(--font-grotesk)",
     },
   },
   plugins: [],
