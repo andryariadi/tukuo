8d1b1f532734b5bd706b88ae77d429611c3d8ce5
Andry Ariadi <andryariadi23@gmail.com>
2024-06-02 19:44:53 +0700

    add search bar comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import { Spotlight } from "@/components/ui/SpotLight";
-
 const HomePage = () => {
   return (
     <div className="bg-amber-500">
-      <Spotlight className="h-screen w-screen" fill="white" />
+      <div>Home Page</div>
     </div>
   );
 };
