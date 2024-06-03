25ab9efebe762f8892fcab26ed50272167daf390
Andry Ariadi <andryariadi23@gmail.com>
2024-06-03 20:08:54 +0700

    update styling product and slider comp

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,82 +3,98 @@ import Link from "next/link";
 
 const ProductList = () => {
   return (
-    <div className="bg-rose-600 flex justify-between flex-wrap gap-x-8 gap-y-16 mt-12">
-      <Link href="/" className="bg-n-8 backdrop-blur-md w-full flex flex-col gap-4 md:w-[45%] lg:w-[22%] p-3 rounded-md border border-n-5 transition-colors duration-500 ease-in-out hover:border-logo">
-        <div className="bg-teal-600 relative w-full h-80">
+    <div className="flex justify-between flex-wrap gap-x-1 gap-y-5 md:gap-y-12 mt-12">
+      <Link href="/" className="bg-n-8 backdrop-blur-md w-full flex flex-col gap-4 md:w-[45%] lg:w-[24%] p-1 rounded-md border border-n-5 transition-colors duration-500 ease-in-out hover:border-logo">
+        {/* Top */}
+        <div className="relative w-full h-[19rem] rounded-t-md">
           <Image
             src="https://images.pexels.com/photos/3913025/pexels-photo-3913025.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt="Product List"
             fill
             sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-all duration-300"
+            className="absolute object-cover rounded-t-md z-10 hover:opacity-0 transition-all duration-300"
           />
 
-          <Image src="https://images.pexels.com/photos/8386363/pexels-photo-8386363.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Product List" fill sizes="25vw" className="absolute object-cover rounded-md" />
+          <Image src="https://images.pexels.com/photos/8386363/pexels-photo-8386363.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Product List" fill sizes="25vw" className="absolute object-cover rounded-t-md" />
         </div>
-        <div className="flex justify-between font-sans">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
+        {/* Bottom */}
+        <div className="bg-tel-500 h-[10rem] flex flex-col items-start justify-between p-1">
+          <div className="w-full flex justify-between font-sans">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+          </div>
+          <p className="text-sm text-n-3">My Description</p>
+          <button className="w-max bg-n-7 py-3 px-4 rounded-md border border-n-1/10 text-n-1">Checkout</button>
         </div>
-        <p className="text-sm text-n-3">My Description</p>
-        <button className="w-max bg-n-7 py-3 px-4 rounded-md border border-n-1/10 text-n-1">Checkout</button>
       </Link>
-      <Link href="/" className="bg-n-8 backdrop-blur-md w-full flex flex-col gap-4 md:w-[45%] lg:w-[22%] p-3 rounded-md border border-n-5 transition-colors duration-500 ease-in-out hover:border-logo">
-        <div className="bg-teal-600 relative w-full h-80">
+      <Link href="/" className="bg-n-8 backdrop-blur-md w-full flex flex-col gap-4 md:w-[45%] lg:w-[24%] p-1 rounded-md border border-n-5 transition-colors duration-500 ease-in-out hover:border-logo">
+        {/* Top */}
+        <div className="bg-teal-600 relative w-full h-[19rem] rounded-t-md">
           <Image
             src="https://images.pexels.com/photos/3913025/pexels-photo-3913025.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt="Product List"
             fill
             sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-all duration-300"
+            className="absolute object-cover rounded-t-md z-10 hover:opacity-0 transition-all duration-300"
           />
 
-          <Image src="https://images.pexels.com/photos/8386363/pexels-photo-8386363.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Product List" fill sizes="25vw" className="absolute object-cover rounded-md" />
+          <Image src="https://images.pexels.com/photos/8386363/pexels-photo-8386363.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Product List" fill sizes="25vw" className="absolute object-cover rounded-t-md" />
         </div>
-        <div className="flex justify-between font-sans">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
+        {/* Bottom */}
+        <div className="bg-tel-500 h-[10rem] flex flex-col items-start justify-between p-1">
+          <div className="w-full flex justify-between font-sans">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+          </div>
+          <p className="text-sm text-n-3">My Description</p>
+          <button className="w-max bg-n-7 py-3 px-4 rounded-md border border-n-1/10 text-n-1">Checkout</button>
         </div>
-        <p className="text-sm text-n-3">My Description</p>
-        <button className="w-max bg-n-7 py-3 px-4 rounded-md border border-n-1/10 text-n-1">Checkout</button>
       </Link>
-      <Link href="/" className="bg-n-8 backdrop-blur-md w-full flex flex-col gap-4 md:w-[45%] lg:w-[22%] p-3 rounded-md border border-n-5 transition-colors duration-500 ease-in-out hover:border-logo">
-        <div className="bg-teal-600 relative w-full h-80">
+      <Link href="/" className="bg-n-8 backdrop-blur-md w-full flex flex-col gap-4 md:w-[45%] lg:w-[24%] p-1 rounded-md border border-n-5 transition-colors duration-500 ease-in-out hover:border-logo">
+        {/* Top */}
+        <div className="bg-teal-600 relative w-full h-[19rem] rounded-t-md">
           <Image
             src="https://images.pexels.com/photos/3913025/pexels-photo-3913025.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt="Product List"
             fill
             sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-all duration-300"
+            className="absolute object-cover rounded-t-md z-10 hover:opacity-0 transition-all duration-300"
           />
 
-          <Image src="https://images.pexels.com/photos/8386363/pexels-photo-8386363.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Product List" fill sizes="25vw" className="absolute object-cover rounded-md" />
+          <Image src="https://images.pexels.com/photos/8386363/pexels-photo-8386363.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Product List" fill sizes="25vw" className="absolute object-cover rounded-t-md" />
         </div>
-        <div className="flex justify-between font-sans">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
+        {/* Bottom */}
+        <div className="bg-tel-500 h-[10rem] flex flex-col items-start justify-between p-1">
+          <div className="w-full flex justify-between font-sans">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+          </div>
+          <p className="text-sm text-n-3">My Description</p>
+          <button className="w-max bg-n-7 py-3 px-4 rounded-md border border-n-1/10 text-n-1">Checkout</button>
         </div>
-        <p className="text-sm text-n-3">My Description</p>
-        <button className="w-max bg-n-7 py-3 px-4 rounded-md border border-n-1/10 text-n-1">Checkout</button>
       </Link>
-      <Link href="/" className="bg-n-8 backdrop-blur-md w-full flex flex-col gap-4 md:w-[45%] lg:w-[22%] p-3 rounded-md border border-n-5 transition-colors duration-500 ease-in-out hover:border-logo">
-        <div className="bg-teal-600 relative w-full h-80">
+      <Link href="/" className="bg-n-8 backdrop-blur-md w-full flex flex-col gap-4 md:w-[45%] lg:w-[24%] p-1 rounded-md border border-n-5 transition-colors duration-500 ease-in-out hover:border-logo">
+        {/* Top */}
+        <div className="bg-teal-600 relative w-full h-[19rem] rounded-t-md">
           <Image
             src="https://images.pexels.com/photos/3913025/pexels-photo-3913025.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt="Product List"
             fill
             sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-all duration-300"
+            className="absolute object-cover rounded-t-md z-10 hover:opacity-0 transition-all duration-300"
           />
 
-          <Image src="https://images.pexels.com/photos/8386363/pexels-photo-8386363.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Product List" fill sizes="25vw" className="absolute object-cover rounded-md" />
+          <Image src="https://images.pexels.com/photos/8386363/pexels-photo-8386363.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Product List" fill sizes="25vw" className="absolute object-cover rounded-t-md" />
         </div>
-        <div className="flex justify-between font-sans">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
+        {/* Bottom */}
+        <div className="bg-tel-500 h-[10rem] flex flex-col items-start justify-between p-1">
+          <div className="w-full flex justify-between font-sans">
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+          </div>
+          <p className="text-sm text-n-3">My Description</p>
+          <button className="w-max bg-n-7 py-3 px-4 rounded-md border border-n-1/10 text-n-1">Checkout</button>
         </div>
-        <p className="text-sm text-n-3">My Description</p>
-        <button className="w-max bg-n-7 py-3 px-4 rounded-md border border-n-1/10 text-n-1">Checkout</button>
       </Link>
     </div>
   );
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -17,12 +17,13 @@ const Slider = () => {
   //   }, []);
 
   return (
-    <div className="relative h-[calc(100vh-5rem)] overflow-hidden">
-      <div className="w-max h-full flex transition-all ease-in-out duration-1000" style={{ transform: `translateX(-${current * 100}vw)` }}>
+    <div className="bg-rose-500 relative h-[calc(100vh-5rem)] overflow-hidden">
+      {/* Item Sliders */}
+      <div className="bg-teal-500 w-max h-full flex transition-all ease-in-out duration-1000" style={{ transform: `translateX(-${current * 100}vw)` }}>
         {slides.map((item) => (
-          <div key={item.id} className={`${item.bg} w-screen h-full flex flex-col xl:flex-row gap-16`}>
+          <div key={item.id} className={`bg-amber-500 w-screen h-full flex flex-col md:flex-row`}>
             {/* Text Container */}
-            <div className="h-1/2 xl:h-full xl:w-1/2 flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
+            <div className="bg-sky-600 h-1/2 md:h-full md:w-1/2 flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
               <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{item.description}</h2>
               <h1 className="text-5xl lg:text-6xl 2xl:text-8xl">{item.title}</h1>
               <Link href={item.url}>
@@ -30,13 +31,13 @@ const Slider = () => {
               </Link>
             </div>
             {/* Image Container */}
-            <div className="relative h-1/2 xl:h-full xl:w-1/2">
+            <div className="bg-gray-500 relative h-1/2 md:h-full md:w-1/2">
               <Image src={item.img} alt={item.title} fill sizes="100%" className="object-cover" />
             </div>
           </div>
         ))}
       </div>
-      {/* Button Slider */}
+      {/* Button Sliders */}
       <div className="absolute left-1/2 bottom-8 flex gap-4">
         {slides.map((item, index) => (
           <div key={item.id} onClick={() => setCurrent(index)} className={`w-3 h-3 flex items-center justify-center rounded-full ring-1 ring-gray-600 cursor-pointer ${current === index ? "scale-150" : ""}`}>
