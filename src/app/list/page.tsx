c662ee475902b129d38bce3f637229be2efea9a4
Andry Ariadi <andryariadi23@gmail.com>
2024-06-04 13:40:37 +0700

    styling list page and filter comp

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,24 +1,30 @@
+import Filter from "@/components/Filter";
+import ProductList from "@/components/ProductList";
 import Image from "next/image";
 
 const ListPage = () => {
   return (
-    <div className="bg-rose-500 px-4 pt-[4.75rem] lg:pt-[5rem] md:px-8 lg:px-16 xl:px-32">
+    <div className="px-4 pt-[4.75rem] md:pt-[6rem] md:px-8 lg:px-16 xl:px-32">
       {/* Campain */}
-      <div className="bg-teal-500 flex justify-between h-64 p-4">
+      <div className="bg-n-7 backdrop-blur-md hidden sm:flex justify-between h-64 p-4 rounded-md border border-n-1/10 transition-colors duration-500 ease-in-out hover:border-logo">
         {/* Text */}
-        <div className="bg-amber-500 w-2/3 flex flex-col items-center justify-center gap-8">
-          <h1 className="text-4xl font-sans font-semibold text-n-3">
+        <div className="w-2/3 flex flex-col items-center justify-center gap-8">
+          <h1 className="text-4xl font-sans font-semibold text-n-3 leading-snug">
             {" "}
             Grab up to 50% off on <br /> Selected Products
           </h1>
           <button className="bg-black text-n-1 uppercase text-sm py-3 px-4 rounded-full border border-n-4 transition-colors duration-500 ease-in-out hover:border-logo hover:text-logo">Buy Now</button>
         </div>
         {/* Image */}
-        <div className="bg-green-700 w-1/3 relative">
+        <div className="w-1/3 relative">
           <Image src="/woman.png" alt="Woman" fill className="object-contain" />
         </div>
       </div>
-      <div className="bg-violet-500">Andry</div>
+      {/* Filter */}
+      <Filter />
+      {/* Products */}
+      <h1 className="mt-12 text-2xl font-sans font-semibold">Shoes For You!</h1>
+      <ProductList />
     </div>
   );
 };
--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,62 @@
 const Filter = () => {
-  return <div>Filter</div>;
+  return (
+    <div className="mt-12 flex flex-col md:flex-row justify-between gap-5 font-sans">
+      <div className="flex gap-6 flex-wrap">
+        {/* Left */}
+        <select name="type" id="" className="bg-n-7 backdrop-blur-md py-2 px-4 rounded-2xl font-medium text-xs text-n-3 border border-n-1/10 transition-colors duration-500 ease-in-out hover:border-logo">
+          <option value="" selected disabled>
+            Type
+          </option>
+          <option value="physical">Physical</option>
+          <option value="digital">Digital</option>
+        </select>
+
+        <input type="text" name="min" placeholder="Min Price" className="bg-n-7 backdrop-blur-md w-24 py-2 px-4 rounded-2xl font-medium text-xs text-n-3 border border-n-1/10 transition-colors duration-500 ease-in-out hover:border-logo" />
+
+        <input type="text" name="max" placeholder="Max Price" className="bg-n-7 backdrop-blur-md w-24 py-2 px-4 rounded-2xl font-medium text-xs text-n-3 border border-n-1/10 transition-colors duration-500 ease-in-out hover:border-logo" />
+
+        <select name="size" id="" className="bg-n-7 backdrop-blur-md py-2 px-4 rounded-2xl font-medium text-xs text-n-3 border border-n-1/10 transition-colors duration-500 ease-in-out hover:border-logo">
+          <option value="" selected disabled>
+            Size
+          </option>
+          <option value="">Size</option>
+        </select>
+
+        <select name="color" id="" className="bg-n-7 backdrop-blur-md py-2 px-4 rounded-2xl font-medium text-xs text-n-3 border border-n-1/10 transition-colors duration-500 ease-in-out hover:border-logo">
+          <option value="" selected disabled>
+            Color
+          </option>
+          <option value="">Color</option>
+        </select>
+
+        <select name="ribbon" id="" className="bg-n-7 backdrop-blur-md py-2 px-4 rounded-2xl font-medium text-xs text-n-3 border border-n-1/10 transition-colors duration-500 ease-in-out hover:border-logo">
+          <option value="" selected disabled>
+            Category
+          </option>
+          <option value="new arrival">New Arrival</option>
+          <option value="popular">Popular</option>
+        </select>
+
+        <select name="" id="" className="bg-n-7 backdrop-blur-md py-2 px-4 rounded-2xl font-medium text-xs text-n-3 border border-n-1/10 transition-colors duration-500 ease-in-out hover:border-logo">
+          <option value="" selected disabled>
+            All Filters
+          </option>
+        </select>
+      </div>
+      {/* Right */}
+      <div className="">
+        <select name="" id="" className="bg-n-7 backdrop-blur-md py-2 px-4 rounded-2xl font-medium text-xs text-n-3 border border-n-1/10 transition-colors duration-500 ease-in-out hover:border-logo">
+          <option value="" selected disabled>
+            Sort By
+          </option>
+          <option value="">Price (low to hight)</option>
+          <option value="">Price (hight to low)</option>
+          <option value="">Newest</option>
+          <option value="">Oldest</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
