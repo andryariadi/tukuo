3d46bd97a199dae3fdfcb78366571d6b0a758fa2
Andry Ariadi <andryariadi23@gmail.com>
2024-06-04 21:15:45 +0700

    styling add component

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,5 +1,46 @@
+"use client";
+
+import { useState } from "react";
+
 const Add = () => {
-  return <div className="bg-rose-500">Add</div>;
+  const [quantity, setQuantity] = useState(1);
+
+  const stock = 4;
+
+  const handleQuantity = (type: "inc" | "dec") => {
+    if (type === "dec" && quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+
+    if (type === "inc" && quantity < stock) {
+      setQuantity(quantity + 1);
+    }
+  };
+
+  return (
+    <div className="bg-rose-500 font-sans flex flex-col gap-4">
+      <h4 className="font-medium">Choose a size</h4>
+
+      <div className="flex justify-between">
+        <div className="flex items-center gap-4">
+          <div className="w-32 bg-teal-500 py-2 px-4 rounded-3xl flex items-center justify-between">
+            <button className="text-xl" onClick={() => handleQuantity("dec")}>
+              -
+            </button>
+            {quantity}
+            <button className="text-xl" onClick={() => handleQuantity("inc")}>
+              +
+            </button>
+          </div>
+          <div className="text-xs">
+            Only <span className="text-orange-500">4 items</span> left! <br /> {"Don't"} miss it
+          </div>
+        </div>
+
+        <button className="w-max bg-black text-n-1 text-sm py-3 px-4 rounded-full border border-n-4 transition-colors duration-500 ease-in-out hover:border-logo hover:text-logo">Add to Cart</button>
+      </div>
+    </div>
+  );
 };
 
 export default Add;
