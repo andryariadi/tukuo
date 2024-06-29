4daf70fb81940f6b9630828bb2f779c48d751ea3
Andry Ariadi <andryariadi23@gmail.com>
2024-06-29 16:03:49 +0700

    update profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,39 @@
-export const ProfilePage = () => {
-  return <div className="flex items-center justify-center h-screen text-n-3 text-2xl">Soon</div>;
+// import UpdateButton from "@/components/UpdateButton";
+// import { updateUser } from "@/libs/actions";
+// import { wixClientServer } from "@/libs/wixClientServer";
+// import { members } from "@wix/members";
+// import Link from "next/link";
+// import { redirect } from "next/navigation";
+// import { CiMail, CiUser } from "react-icons/ci";
+// import { PiPhoneThin } from "react-icons/pi";
+// import { RiUserReceivedLine, RiUserSharedLine } from "react-icons/ri";
+
+const ProfilePage = async () => {
+  // const wixClient = await wixClientServer();
+
+  // const user = await wixClient.members.getCurrentMember({
+  //   fieldsets: [members.Set.FULL],
+  // });
+
+  // if (!user || !user.member || !user.member.contactId) {
+  //   console.error("User data is missing or incomplete", user);
+  //   redirect("/");
+  // }
+
+  // const orderRes = await wixClient.orders.searchOrders({
+  //   search: {
+  //     filter: { "buyerInfo.contactId": { $eq: user.member.contactId } },
+  //   },
+  // });
+
+  // console.log(user, orderRes, "<----diprofilepage");
+
+  return (
+    <div className="bg-ros-500 px-4 pt-[4.75rem] md:pt-[8rem] md:px-8 lg:px-16 xl:px-32 mt-10 flex flex-col lg:flex-row gap-20 font-sans text-n-3">
+      {/* User Information */}
+      <p className="text-2xl text-n-3">Soon</p>
+    </div>
+  );
 };
+
+export default ProfilePage;
